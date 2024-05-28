73ac556c4ff7b319f2c79f42b0bef0726577cf29
Lalu USamah Wahidil Qahar <a253d4ky4208@bangkit.academy>
2024-05-28 15:16:40 +0800

    add authorization

---
+++ app/src/main/java/com/example/freshgrade/ui/splash/SplashActivity.kt
@@ -0,0 +1,24 @@
+package com.example.freshgrade.ui.splash
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.freshgrade.R
+import com.example.freshgrade.ui.authorization.AuthActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash)
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this@SplashActivity, AuthActivity::class.java))
+            finish()
+        }, 2000)
+    }
+}
