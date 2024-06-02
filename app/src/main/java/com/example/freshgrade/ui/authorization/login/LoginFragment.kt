f266e7cc40609bc4abd5a1cc9d39fc03fceaa7ac
Lalu USamah Wahidil Qahar <a253d4ky4208@bangkit.academy>
2024-06-02 20:04:08 +0800

    Login Register Design

--- app/src/main/java/com/example/freshgrade/ui/authorization/login/LoginFragment.kt
+++ app/src/main/java/com/example/freshgrade/ui/authorization/login/LoginFragment.kt
@@ -5,12 +5,11 @@ import android.animation.ObjectAnimator
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
+import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import androidx.navigation.fragment.findNavController
 import com.cscorner.storyapp.ui.customview.EmailEdit
@@ -19,16 +18,7 @@ import com.cscorner.storyapp.ui.customview.PasswordEdit
 import com.example.freshgrade.R
 import com.example.freshgrade.databinding.FragmentLoginBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [LoginFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class LoginFragment : Fragment() {
     private var _binding: FragmentLoginBinding? = null
     private val binding get() = _binding!!
@@ -185,16 +175,8 @@ class LoginFragment : Fragment() {
         _binding = null
     }
 
-
-
     companion object {
 
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            LoginFragment().apply {
-                arguments = Bundle().apply {
 
-                }
-            }
     }
 }
--- app/src/main/java/com/example/freshgrade/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/example/freshgrade/ui/splash/SplashActivity.kt
@@ -1,16 +1,16 @@
 package com.example.freshgrade.ui.splash
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.freshgrade.R
 import com.example.freshgrade.ui.authorization.AuthActivity
 
+@SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
