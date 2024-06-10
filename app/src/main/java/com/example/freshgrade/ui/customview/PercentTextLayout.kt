3980647951f0151d9406f0230bd3300f3ba2ac13
Lalu USamah Wahidil Qahar <a253d4ky4208@bangkit.academy>
2024-06-10 21:31:41 +0800

    result design

---
+++ app/src/main/java/com/example/freshgrade/ui/customview/PercentTextLayout.kt
@@ -0,0 +1,45 @@
+package com.example.freshgrade.ui.customview
+
+import android.content.Context
+import android.os.Build
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.example.freshgrade.R
+
+class PercentTextLayout @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : ConstraintLayout(context, attrs, defStyleAttr) {
+
+    private val textView: TextView
+
+    init {
+        LayoutInflater.from(context).inflate(R.layout.view_percent_text_layout, this, true)
+        textView = findViewById(R.id.fruitFreshnesText)
+        background = resources.getDrawable(R.drawable.percent_circle_red_bg, context.theme)
+
+        // Initialize with the default background color
+        updateBackgroundColor()
+    }
+
+    fun setText(percentage: String) {
+        textView.text = percentage
+        updateBackgroundColor()
+    }
+
+    private fun updateBackgroundColor() {
+        val percentage = textView.text.toString().replace("%", "").toIntOrNull() ?: 0
+        if (percentage < 50) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                setBackgroundColor(resources.getColor(R.color.red, context.theme))
+            }
+        } else {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                setBackgroundColor(resources.getColor(R.color.green, context.theme))
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/freshgrade/ui/main/result/ResultViewModel.kt
+++ app/src/main/java/com/example/freshgrade/ui/main/result/ResultViewModel.kt
@@ -1,7 +1,15 @@
 package com.example.freshgrade.ui.main.result
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class ResultViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    private val _fruitFreshness = MutableLiveData<String>()
+    val fruitFreshness: LiveData<String> get() = _fruitFreshness
+
+    // Function to update the freshness value
+    fun updateFreshness(freshness: String) {
+        _fruitFreshness.value = freshness
+    }
 }
