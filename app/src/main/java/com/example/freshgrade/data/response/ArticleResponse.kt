2e5de8fd52ef2a1bcf289d39480b16775e223fde
Seloaziz <seloaziz21@gmail.com>
2024-06-15 23:03:34 +0700

    Finish Article feature second try

--- app/src/main/java/com/example/freshgrade/data/response/ArticleResponse.kt
+++ app/src/main/java/com/example/freshgrade/data/response/ArticleResponse.kt
@@ -1,4 +1,30 @@
 package com.example.freshgrade.data.response
 
-class ArticleResponse {
-}
+import com.google.gson.annotations.SerializedName
+import java.util.Date
+
+data class ArticleResponse(
+    @field:SerializedName("id")
+    val id: String? = null,
+
+    @field:SerializedName("createdAt")
+    val createdAt: Date? = null,
+
+    @field:SerializedName("title")
+    val title: String? = null,
+
+    @field:SerializedName("url")
+    val url: String? = null,
+
+    @field:SerializedName("uploadDate")
+    val uploadDate: Date? = null,
+
+    @field:SerializedName("author")
+    val author: String? = null,
+
+    @field:SerializedName("thumbnailUrl")
+    val thumbnailUrl: String? = null,
+
+    @field:SerializedName("type")
+    val type: String? = null
+)
